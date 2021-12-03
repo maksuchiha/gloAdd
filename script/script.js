44287ddeb5728e101fcbdc66d27b5032cddded9f
Maksim Sheko <uchihaboy95@gmail.com>
2021-12-03 20:44:14 +0300

    style rand in randomInteger

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 "use strict"
 
 function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+    const rand = min + Math.random() * (max + 1 - min)
+    return Math.floor(rand)
 }
 
 const isNumber = (num) => {
